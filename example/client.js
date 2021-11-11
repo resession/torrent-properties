ae71b93fe75f9fac51389e08e52789a1db16b79b
resession <plainstud@gmail.com>
2021-11-11 00:29:42 -0600

    make change

---
+++ example/client.js
@@ -0,0 +1,19 @@
+const WebTorrentProperties = require('./index')
+
+const client = new WebTorrentProperties(false)
+
+client.on('error', error => {
+    console.log(error)
+})
+
+const path = require('path')
+
+client.uploadProp({address: '50105b102fa4e5c666fbdb218393aee25d295c4c1de0e45c814e1ec1fa47c6cc', secret: '928d3dec6b3aa7fe51a553f77d3a87c4ff5fbf4692c03e458996afce754d882950105b102fa4e5c666fbdb218393aee25d295c4c1de0e45c814e1ec1fa47c6cc'}, false, path.resolve('./test'), {}, (torrent, data) => {
+    console.log(torrent.address)
+    console.log(data)
+})
+
+// client.downloadProp('50105b102fa4e5c666fbdb218393aee25d295c4c1de0e45c814e1ec1fa47c6cc', false, {path: __dirname + '/maintest'}, (torrent, data) => {
+//     console.log(torrent)
+//     console.log(data)
+// })
---
+++ example/extend.js
@@ -0,0 +1,46 @@
+const Webtorrent = require('webtorrent')
+const Properties = require('torrent-properties')
+const sodium = require('sodium-universal')
+
+function verify (signature, message, publicKey) {
+    return sodium.crypto_sign_verify_detached(signature, message, publicKey)
+  }
+
+class WebTorrentProperties extends Webtorrent {
+    constructor(options){
+        super(options || {dht: {verify}})
+        this.properties = new Properties(this.dht)
+    }
+    downloadProp(address, manage, opts, callback){
+        this.properties.resolve(address, manage, (resError, propData) => {
+            if(resError){
+                console.log(resError)
+                return this.emit(resError)
+            } else {
+                this.add(propData.infoHash, opts, torrent => {
+                    torrent.address = propData.address
+                    torrent.sequence = propData.sequence
+                    torrent.own = propData.own
+                    return callback(torrent, propData)
+                })
+            }
+        })
+    }
+    uploadProp(keypair, manage, data, opts, callback){
+        this.seed(data, opts, torrent => {
+            this.properties.publish(keypair, torrent.infoHash, manage, (resError, propData) => {
+                if(resError){
+                    console.log(resError)
+                    return this.emit(resError)
+                } else {
+                    torrent.address = propData.address
+                    torrent.sequence = propData.sequence
+                    torrent.own = propData.own
+                    return callback(torrent, propData)
+                }
+            })
+        })
+    }
+}
+
+module.exports = WebTorrentProperties
